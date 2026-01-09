8642c22617f8431e6d89a3a436d06235cf7c03e2
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-09 11:54:46 -0800

    added new html pages, project setup

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -4,14 +4,19 @@ import { dbConnection } from "../db/db.js";
 export async function getGenres(req, res) {
   try {
     const db = await dbConnection();
+
     const allGenres = await db.all(`SELECT DISTINCT genre FROM products`);
+
     const genreList = allGenres.map(({genre}) => genre)
+
     return res.json(genreList)
   } catch (err) {
+    
     return res.status(500).json({
       error: 'Failed to fetch genres', 
       details: err.message
     })
+
   } 
 }
 
@@ -40,9 +45,11 @@ export async function getProducts(req, res) {
    
     res.json(products)
   } catch (err) {
+
     return res.status(500).json({
       error: 'Failed to fetch products', 
       details: err.message
     })
+    
   }
 }
