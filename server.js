bd6d95c3591ee85a5279afdce264e4f48810c50b
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-09 01:00:14 -0800

    project setup

---
+++ server.js
@@ -0,0 +1,18 @@
+import express from "express";
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static("public"));
+
+app.use((req, res) => {
+  res.status(404).json({
+    message: "Page Not Found, sorry."
+  })
+})
+
+app.listen(PORT, () => {
+  console.log(`Server running at http://localhost:${PORT}`)
+}).on('error', (err) => {
+  console.error('Failed to start server:', err)
+}) 
