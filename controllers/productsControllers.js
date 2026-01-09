c130d6d2b7fde352c61a8568b1f6ca5ac13c34af
DarrylAdigwu <115912161+DarrylAdigwu@users.noreply.github.com>
2026-01-09 11:47:09 -0800

    added filter with path and query params

--- controllers/productsControllers.js
+++ controllers/productsControllers.js
@@ -6,16 +6,43 @@ export async function getGenres(req, res) {
     const db = await dbConnection();
     const allGenres = await db.all(`SELECT DISTINCT genre FROM products`);
     const genreList = allGenres.map(({genre}) => genre)
-    console.log(genreList)
-    res.json(genreList)
+    return res.json(genreList)
   } catch (err) {
-    // return res.status(500).json({
-    //   error: 'Failed to fetch genres', 
-    //   details: err.message
-    // })
-  }
+    return res.status(500).json({
+      error: 'Failed to fetch genres', 
+      details: err.message
+    })
+  } 
 }
-getGenres()
-export async function getProducts() {
-  console.log("products")
+
+export async function getProducts(req, res) {
+  
+  try {
+    const { genre, search } = req.query;
+   
+    const db = await dbConnection();
+
+    let productsQuery = `SELECT * FROM products`
+
+    let productsInput = [];
+
+    if (genre) {
+      productsQuery += ` WHERE genre = ?`
+      productsInput.push(genre)
+    } else if (search) {
+      productsQuery += ` WHERE 
+      (title || artist || genre)
+      LIKE ?`
+      productsInput.push(`%${search}%`)
+    }
+
+    const products = await db.all(productsQuery, productsInput);
+   
+    res.json(products)
+  } catch (err) {
+    return res.status(500).json({
+      error: 'Failed to fetch products', 
+      details: err.message
+    })
+  }
 }
